dd13029eebc45a27ccf231b47afe2e2956309c30
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-06 15:03:06 +0300

    initial commit

---
+++ src/components/TransactionRow.js
@@ -0,0 +1,18 @@
+
+
+const TransactionRow = () => {
+  return (
+    <tr>
+    <td>None</td>
+    <td>Salary</td>
+    <td>Income</td>
+    <td>10000</td>
+    <td>
+      <button>Edit</button>
+      <button>Delete</button>
+    </td>
+  </tr>
+  );
+}
+
+export default TransactionRow;
